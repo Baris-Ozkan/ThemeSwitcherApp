10df270d97a501e0973feb8537b414cd9f1b8115
Baris Ozkan <bozkan01@students.cs.ubc.ca>
2022-08-08 17:48:06 +0300

    Pushing project to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ThemeService} from "./core/services/theme.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'theme-app';
+
+  constructor(private themeService: ThemeService) {
+  }
+
+  ngOnInit() {
+    let savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme != null) {
+      this.themeService.switchTheme(savedTheme);
+    }
+  }
+
+  changeTheme(theme: string) {
+    this.themeService.switchTheme(theme);
+  }
 }
---
+++ src/app/core/services/theme.service.ts
@@ -0,0 +1,19 @@
+import {Inject, Injectable} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  switchTheme(theme: string) {
+    let themeLink = this.document.getElementById('theme-app') as HTMLLinkElement;
+
+    if (themeLink) {
+      themeLink.href = theme + '.css';
+      localStorage.setItem('theme', theme);
+    }
+  }
+}
